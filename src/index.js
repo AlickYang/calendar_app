082e49d33fee69b9cb07f0eee181cdcd62cb8b5f
Alick <alick.yang96@gmail.com>
2019-07-31 15:58:54 +1000

    added error checking in mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./typeDefs";
-import express from "express";
 import mongoose from "mongoose";
 import resolvers from "./resolvers";
-import { APP_PORT, IN_PROD, MONGO_URI, NODE_ENV } from "./config/keys";
-import cors from "cors";
+import { IN_PROD, MONGO_URI, NODE_ENV } from "./config/keys";
 
 const server = new ApolloServer({
   typeDefs,
@@ -14,25 +12,12 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req })
 });
 
-// mongoose
-//   .connect(MONGO_URI)
-//   .then(() => console.log("Connected to MongoDB.."))
-//   .catch(err => console.log("Error: " + err));
-
-// const app = express();
-// //Allow Cors
-// app.use(cors());
-// app.disable("x-powered-by");
-// server.applyMiddleware({ app, cors: false });
-
-// app.listen({ port: APP_PORT }, () =>
-//   console.log(
-//     `🚀 Server ready at http://localhost:${APP_PORT}${server.graphqlPath}`
-//   )
-// );
-
-mongoose.connect(MONGO_URI, { useNewUrlParser: true }).then(() => {
-  server.listen({ port: APP_PORT }).then(res => {
-    console.log(`Server ready at ${res.url}`);
+try {
+  await mongoose.connect(MONGO_URI, { useNewUrlParser: true }).then(() => {
+    server.listen({ port: APP_PORT }).then(res => {
+      console.log(`Server ready at ${res.url}`);
+    });
   });
-});
+} catch (err) {
+  console.log(err);
+}
